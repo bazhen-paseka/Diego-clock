562b75b3193110d0d8308421a180c4affb5f6685
bazhen.paseka <bazhen.paseka@gmail.com>
2025-05-05 15:45:44 +0300

    only Local_config

--- Drivers/Local_config/Local_config.h
+++ Drivers/Local_config/Local_config.h
@@ -3,10 +3,7 @@
 #define DIGITAL_CLOCK_FOR_BEDROOM_CONFIG_INCLUDED_H
 //******************************************************************************************
 
-	#define SOFT_VERSION	1920
-	#define VERSION_YEAR	2024
-	#define VERSION_MONTH	10
-	#define VERSION_DAY		28
+	#define SOFT_VERSION	200
 
 	#define	BUTTON_DELAY	100
 	#define	BEEPER_DELAY	 50
@@ -14,8 +11,20 @@
 	#define LIGHT_LEVEL_MIN		3500LU
 	#define LIGHT_LEVEL_MAX		3600LU
 
-	#define START_NIGHT_MODE_HOUR	23
-	#define FINISH_NIGHT_MODE_HOUR	 3
+	#define START_NIGHT_MODE_HOUR	22
+	#define FINISH_NIGHT_MODE_HOUR	 6
+
+	#define ADC_MODULE
+	#define ADR_I2C_DS3231 		0x68
+
+	#define INTENSITY_INIT	Intensity_31
+	#define INTENSITY_MIN	Intensity_1
+	#define INTENSITY_MAX	Intensity_15
+	#define DECODE_MODE		NoDecode
+	#define DISPLAY_DIGIT	DisplayDigit_0_7
+	#define WORK_MODE		NormalOperation
+	#define OFF_MODE		ShutdownMode
+	#define	RANDOM_DOT
 
 //******************************************************************************************
 #endif	//	DIGITAL_CLOCK_FOR_BEDROOM_CONFIG_INCLUDED_H
